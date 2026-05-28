9b744c5fe04db31ced7f161f362cdcd73f1d6878
zoobavitel <criszbav@gmail.com>
2026-05-28 12:21:21 -0400

    system-configs: add waybar-resume, remove supergfx-sleep from install

--- .local/share/system-configs/install.sh
+++ .local/share/system-configs/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo cp etc/systemd/system/sddm-resume.service /etc/systemd/system/
-sudo cp etc/systemd/system/supergfx-sleep.service /etc/systemd/system/
+sudo cp etc/systemd/system/waybar-resume.service /etc/systemd/system/
 sudo cp etc/systemd/sleep.conf.d/hibernate.conf /etc/systemd/sleep.conf.d/
 # Boot entries intentionally excluded - set resume= manually after install
 echo "Remember to add resume=UUID=... resume_offset=4814848 to bootloader entries"
 sudo systemctl daemon-reload
-sudo systemctl enable sddm-resume.service supergfx-sleep.service
+sudo systemctl enable sddm-resume.service waybar-resume.service
