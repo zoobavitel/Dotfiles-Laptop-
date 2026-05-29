085b8910dca1f43b021f94641a87a549697c9ea6
zoobavitel <criszbav@gmail.com>
2026-05-29 09:28:03 -0400

    system-configs: remove waybar-resume.service, waybar survives hibernate on its own

--- .local/share/system-configs/install.sh
+++ .local/share/system-configs/install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 sudo cp etc/systemd/system/sddm-resume.service /etc/systemd/system/
-sudo cp etc/systemd/system/waybar-resume.service /etc/systemd/system/
 sudo cp etc/systemd/sleep.conf.d/hibernate.conf /etc/systemd/sleep.conf.d/
 # Boot entries intentionally excluded - set resume= manually after install
 echo "Remember to add resume=UUID=... resume_offset=4814848 to bootloader entries"
 sudo systemctl daemon-reload
-sudo systemctl enable sddm-resume.service waybar-resume.service
+sudo systemctl enable sddm-resume.service
